6ccc41068fe30580b63244c91ae0d571d7cdfccd
ianisra <90713096+ianisra@users.noreply.github.com>
2021-11-10 18:49:54 -0600

    Create E2.psm1

---
+++ Powershell/E2/E2.psm1
@@ -0,0 +1,38 @@
+#Ver-StatusPerfil 
+
+function Ver-StatusPerfil{  
+    param([Parameter(Mandatory)] [ValidateSet("Public","Private")] [string] $perfil)  
+    $status = Get-NetFirewallProfile -Name $perfil  
+    Write-Host "Perfil:" $perfil  
+    if($status.enabled){  
+        Write-Host "Status: Activado"  
+    } else{  
+        Write-Host "Status: Desactivado"  
+    }  
+}  
+
+ 
+
+#Cambiar-StatusPerfil 
+function Cambiar-StatusPerfil{  
+    param([Parameter(Mandatory)] [ValidateSet("Public","Private")] [string] $perfil)  
+    $status = Get-NetFirewallProfile -Name $perfil  
+    Write-Host "Perfil:" $perfil  
+    if($status.enabled){  
+        Write-Host "Status actual: Activado"  
+        $opc = Read-Host -Promt "Deseas desactivarlo? [Y] Si [N] No"  
+        if ($opc -eq "Y"){  
+            Set-NetFirewallProfile -Name $perfil -Enabled False  
+        }  
+    } else{  
+        Write-Host "Status: Desactivado"  
+        $opc = Read-Host -Promt "Deseas activarlo? [Y] Si [N] No"  
+        if ($opc -eq "Y"){  
+            Write-Host "Activando perfil"  
+            Set-NetFirewallProfile -Name $perfil -Enabled True  
+        }  
+    }  
+    Ver-StatusPerfil -perfil $perfil  
+}  
+
+ 
